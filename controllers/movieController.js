75c4d1d4fdffe2c8cf9a52b65fba1c7ae99f3fa5
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-04-17 17:46:38 +0200

    Add store function to movie controller for adding reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -36,7 +36,26 @@ function show(req, res) {
     })
 }
 
+function store(req, res) {
+    const id = Number(req.params.id);
+    const { name, vote, text } = req.body;
+
+    const created_at = new Date().toISOString().slice(0, 19).replace('T', ' ')
+    const updated_at = created_at
+
+    const insertSql = 'INSERT INTO reviews (movie_id, name, vote, text, created_at, updated_at) VALUES (?, ?, ?, ?, ? ,?)'
+    const values = [id, name, vote, text, created_at, updated_at]
+
+    connection.query(insertSql, values, (err, results) => {
+        if (err) return res.status(500).json({ error: err.message })
+
+        console.log(results);
+        res.status(201).json({ message: 'Review added successfully', reviewId: results.insertId })
+    })
+}
+
 module.exports = {
     index,
-    show
+    show,
+    store
 }
--- routers/movies.js
+++ routers/movies.js
@@ -8,4 +8,7 @@ router.get('/', MovieController.index)
 /* Show */
 router.get('/:id', MovieController.show)
 
+/* Store */
+router.post('/:id/review', MovieController.store)
+
 module.exports = router;
